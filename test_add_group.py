f386f9c347e6ec1a021644891deecec35a49edf0
yana <kisha_92@mail.ru>
2021-10-04 23:42:13 +0300

    code refactoring

--- test_add_group.py
+++ test_add_group.py
@@ -8,32 +8,25 @@ class UntitledTestCase(unittest.TestCase):
         self.wd.implicitly_wait(30)
     
     def test_untitled_test_case(self):
-        wd = self.open_home_page()
-        wd.get("http://localhost/addressbook/index.php")
+        wd = self.wd
+        self.open_home_page(wd)
         self.login(wd)
         self.create_group(wd)
         self.return_to_groups_page(wd)
         self.logout(wd)
 
-    def open_home_page(self):
-        wd = self.wd
-        return wd
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
 
-    def login(self, wd):
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
+    def return_to_groups_page(self, wd):
+        wd.find_element_by_link_text("group page").click()
 
     def create_group(self, wd):
         # open groups page
         wd.find_element_by_link_text("groups").click()
         # init group creation
         wd.find_element_by_name("new").click()
-        # fill group form
+        # fill group name
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys("Test")
@@ -46,11 +39,17 @@ class UntitledTestCase(unittest.TestCase):
         # submit group creation
         wd.find_element_by_name("submit").click()
 
-    def return_to_groups_page(self, wd):
-        wd.find_element_by_link_text("group page").click()
+    def login(self, wd):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//input[@value='Login']").click()
 
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/index.php")
 
     def tearDown(self):
         self.wd.quit()
