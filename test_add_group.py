8920f593e7db41e52abe7c4a85e82113718d3dba
yana <kisha_92@mail.ru>
2021-10-04 23:59:27 +0300

    add test_add_empty_group

--- test_add_group.py
+++ test_add_group.py
@@ -15,6 +15,14 @@ class UntitledTestCase(unittest.TestCase):
         self.return_to_groups_page(wd)
         self.logout(wd)
 
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.create_group(wd, name="", header="", footer="")
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
 
