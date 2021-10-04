28858d8f9a8b7a3f858df11983b58784484a27ee
yana <kisha_92@mail.ru>
2021-10-05 01:13:44 +0300

    add test add_contact

---
+++ test_add_contact.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+class UntitledTestCase(unittest.TestCase):
+    def setUp(self):
+        self.wb = webdriver.Firefox()
+        self.wb.implicitly_wait(30)
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_untitled_test_case(self):
+        wb = self.wb
+        self.open_main_page(wb)
+        self.login(wb)
+        self.create_contact(wb)
+        self.return_to_main_page(wb)
+        self.logout(wb)
+
+    def logout(self, wb):
+        # logout
+        wb.find_element_by_link_text("Logout").click()
+
+    def return_to_main_page(self, wb):
+        # return to main page
+        wb.find_element_by_link_text("home").click()
+
+    def create_contact(self, wb):
+        # init contact creation
+        wb.find_element_by_link_text("add new").click()
+        # fill contact info
+        wb.find_element_by_name("firstname").click()
+        wb.find_element_by_name("firstname").clear()
+        wb.find_element_by_name("firstname").send_keys("Test First name")
+        wb.find_element_by_name("middlename").click()
+        wb.find_element_by_name("middlename").clear()
+        wb.find_element_by_name("middlename").send_keys("Tast Middle name")
+        wb.find_element_by_name("lastname").click()
+        wb.find_element_by_name("lastname").clear()
+        wb.find_element_by_name("lastname").send_keys("Test Last name")
+        wb.find_element_by_name("nickname").click()
+        wb.find_element_by_name("nickname").clear()
+        wb.find_element_by_name("nickname").send_keys("Test Nickname")
+        wb.find_element_by_name("title").click()
+        wb.find_element_by_name("title").clear()
+        wb.find_element_by_name("title").send_keys("Test title")
+        wb.find_element_by_name("company").click()
+        wb.find_element_by_name("company").clear()
+        wb.find_element_by_name("company").send_keys("Test Company")
+        wb.find_element_by_name("address").click()
+        wb.find_element_by_name("address").clear()
+        wb.find_element_by_name("address").send_keys("Test Address")
+        wb.find_element_by_name("home").click()
+        wb.find_element_by_name("home").clear()
+        wb.find_element_by_name("home").send_keys("+74951111111")
+        wb.find_element_by_name("mobile").click()
+        wb.find_element_by_name("mobile").clear()
+        wb.find_element_by_name("mobile").send_keys("+79999999999")
+        wb.find_element_by_name("work").click()
+        wb.find_element_by_name("work").clear()
+        wb.find_element_by_name("work").send_keys("Test Work")
+        wb.find_element_by_name("fax").click()
+        wb.find_element_by_name("fax").clear()
+        wb.find_element_by_name("fax").send_keys("Test Fax")
+        wb.find_element_by_name("email").click()
+        wb.find_element_by_name("email").clear()
+        wb.find_element_by_name("email").send_keys("Test email")
+        wb.find_element_by_name("homepage").click()
+        wb.find_element_by_name("homepage").clear()
+        wb.find_element_by_name("homepage").send_keys("Test Homepage")
+        wb.find_element_by_name("address2").click()
+        wb.find_element_by_name("address2").clear()
+        wb.find_element_by_name("address2").send_keys("Test Address")
+        wb.find_element_by_name("phone2").click()
+        wb.find_element_by_name("phone2").clear()
+        wb.find_element_by_name("phone2").send_keys("Home")
+        wb.find_element_by_name("notes").click()
+        wb.find_element_by_name("notes").clear()
+        wb.find_element_by_name("notes").send_keys("Test Notes")
+        wb.find_element_by_id("content").click()
+        # submit contact creation
+        wb.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+
+    def login(self, wb):
+        # login
+        wb.find_element_by_name("user").click()
+        wb.find_element_by_name("user").clear()
+        wb.find_element_by_name("user").send_keys("admin")
+        wb.find_element_by_name("pass").click()
+        wb.find_element_by_name("pass").clear()
+        wb.find_element_by_name("pass").send_keys("secret")
+        wb.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_main_page(self, wb):
+        # open main page
+        wb.get("http://localhost/addressbook/index.php")
+
+    def tearDown(self):
+        self.wb.quit()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
