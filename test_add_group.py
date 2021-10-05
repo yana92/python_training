e2205ef78776004accd5b9780e6b8eb55098a4bf
yana <kisha_92@mail.ru>
2021-10-05 19:39:19 +0300

    Improved code readability

--- test_add_group.py
+++ test_add_group.py
@@ -10,18 +10,14 @@ class UntitledTestCase(unittest.TestCase):
     
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.create_group(wd, Group(name="Test_name", header="Test_header", footer="Test_footer"))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def test_add_empty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.create_group(wd, Group(name="", header="", footer=""))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def logout(self, wd):
@@ -31,8 +27,7 @@ class UntitledTestCase(unittest.TestCase):
         wd.find_element_by_link_text("group page").click()
 
     def create_group(self, wd, group):
-        # open groups page
-        wd.find_element_by_link_text("groups").click()
+        self.open_groups_page(wd)
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group name
@@ -47,8 +42,13 @@ class UntitledTestCase(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
+        self.return_to_groups_page(wd)
+
+    def open_groups_page(self, wd):
+        wd.find_element_by_link_text("groups").click()
 
     def login(self, wd, username, password):
+        self.open_home_page(wd)
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
