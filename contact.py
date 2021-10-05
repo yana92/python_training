c0fef7009d2399b6344e861e7623b6554e98fb5f
yana <kisha_92@mail.ru>
2021-10-05 13:46:03 +0300

    create class Contact

---
+++ contact.py
@@ -0,0 +1,20 @@
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home_phone, mobile,
+                       work, fax, email, homepage, address2, phone2, notes):
+        self.firstname=firstname
+        self.middlename=middlename
+        self.lastname=lastname
+        self.nickname=nickname
+        self.title=title
+        self.company=company
+        self.address=address
+        self.home_phone=home_phone
+        self.mobile=mobile
+        self.work=work
+        self.fax=fax
+        self.email=email
+        self.homepage=homepage
+        self.address2=address2
+        self.phone2=phone2
+        self.notes=notes
