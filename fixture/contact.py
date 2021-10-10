372c3bcca10eaae603b22101668d3af47b0586f6
yana <kisha_92@mail.ru>
2021-10-10 23:31:21 +0300

    Добавлен тест test_del_contact

--- fixture/contact.py
+++ fixture/contact.py
@@ -59,3 +59,10 @@ class ContactHelper:
         wd.find_element_by_id("content").click()
         # submit contact creation
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+
+    def delete_first_contact(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_xpath('//input[@value="Delete"]').click()
+        wd.switch_to_alert().accept()
+        wd.find_element_by_id("maintable")
