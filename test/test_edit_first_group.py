da1cc264e143a01bf9e64ff86d3dc2deab21d477
yana <kisha_92@mail.ru>
2021-10-14 00:55:15 +0300

    удален лишний logout в тесте

--- test/test_edit_first_group.py
+++ test/test_edit_first_group.py
@@ -3,7 +3,6 @@ from model.group import Group
 
 def test_edit_group_name(app):
     app.group.edit_group(Group(name="New name"))
-    app.session.logout()
 
 def test_edit_group_header(app):
     app.group.edit_group(Group(header="New header"))
