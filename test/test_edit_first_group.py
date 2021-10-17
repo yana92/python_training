4d61b3eb068c3a57fb456d13e2e8f06306899c56
yana <kisha_92@mail.ru>
2021-10-17 23:44:11 +0300

    Добавлена проверка на количество групп

--- test/test_edit_first_group.py
+++ test/test_edit_first_group.py
@@ -4,9 +4,15 @@ from model.group import Group
 def test_edit_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.edit_group(Group(name="New name"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 def test_edit_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(header="test header"))
+    old_groups = app.group.get_group_list()
     app.group.edit_group(Group(header="New header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
