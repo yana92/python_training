d66a2adf85b864e0c689fd7d178d1db09f4dd58b
yana <kisha_92@mail.ru>
2021-10-25 01:48:45 +0300

    Добавлен тест на сравнение данных конаткта на главной странице и на странице редактирования

--- fixture/contact.py
+++ fixture/contact.py
@@ -101,10 +101,11 @@ class ContactHelper:
                 cells = element.find_elements_by_xpath("td")
                 lastname = element.find_element_by_xpath("td[2]").text
                 firstname = element.find_element_by_xpath("td[3]").text
+                address = element.find_element_by_xpath("td[4]").text
                 id = element.find_element_by_name("selected[]").get_attribute("value")
                 all_phones = cells[5].text
                 all_emails = cells[4].text
-                self.contact_cache.append(Contact(lastname=lastname, firstname=firstname, id=id,
+                self.contact_cache.append(Contact(lastname=lastname, firstname=firstname, id=id, address=address,
                                                   all_phones_from_home_page=all_phones,
                                                   all_emails_from_home_page=all_emails))
         return list(self.contact_cache)
@@ -119,11 +120,13 @@ class ContactHelper:
         work_phone = wd.find_element_by_name("work").get_attribute("value")
         mobile = wd.find_element_by_name("mobile").get_attribute("value")
         phone2 = wd.find_element_by_name("phone2").get_attribute("value")
+        address = wd.find_element_by_name("address").text
         email = wd.find_element_by_name("email").get_attribute("value")
         email2 = wd.find_element_by_name("email2").get_attribute("value")
         email3 = wd.find_element_by_name("email3").get_attribute("value")
+
         return Contact(firstname=firstname, lastname=lastname, id=id, home_phone=home_phone, work_phone=work_phone,
-                       mobile=mobile, phone2=phone2, email=email, email2=email2, email3=email3)
+                       mobile=mobile, phone2=phone2, address=address, email=email, email2=email2, email3=email3)
 
     def get_contact_from_view_page(self, index):
         wd = self.app.wd
---
+++ test/test_contacts_info.py
@@ -0,0 +1,30 @@
+from model.contact import Contact
+import re
+#Осталось реализовать выбор рандомного контакта
+
+def test_contacts_info_on_home_page(app):
+    if app.contact.count() == 0:
+        app.contact.create_contact(Contact(lastname="Test Lastname", firstname="Firstname", address="Test addres",
+                                           home_phone="+74951111111", mobile="+79999999999", work_phone="Test Work",
+                                           email="Test email", phone2="Home"))
+    contact_from_home_page = app.contact.get_contact_list()[0]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
+    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
+    assert contact_from_home_page.address == contact_from_edit_page.address
+    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
+
+
+def clear(s):
+    return re.sub("[() -]", "", s)
+
+def merge_phones_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "",
+                            map(lambda x: clear(x),
+                                filter(lambda x: x is not None,
+                                [contact.home_phone, contact.mobile, contact.work_phone, contact.phone2]))))
+
+def merge_emails_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x!= "",
+                     [contact.email, contact.email2, contact.email3]))
