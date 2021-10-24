789b765da1ba4fad05a3b16e94012a75cec93edf
yana <kisha_92@mail.ru>
2021-10-25 01:21:01 +0300

    Убран ненужный импорт

--- test/test_emails.py
+++ test/test_emails.py
@@ -1,5 +1,3 @@
-import re
-
 def test_emails_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
