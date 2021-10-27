9a252e84f854e6056e02d3fbe5e598cb514199bc
yana <kisha_92@mail.ru>
2021-10-27 18:05:36 +0300

    Добавлена генерация случайных данных в тест создания группы

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,11 +1,23 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+import pytest
+import random
+import string
 
-def test_add_group(app):
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range (random.randrange(maxlen))])
+
+testdata = [Group(name="", header="", footer="")] + [
+    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string(("footer", 20)))
+    for i in range(5)
+]
+
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_add_group(app, group):
     old_groups = app.group.get_group_list()
-    group = Group(name="Test_name", header="Test_header", footer="Test_footer")
     app.group.create(group)
-    new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
     old_groups.append(group)
