dc766abd3afa7b5728f9400b071ab48c7d1ac25d
yana <kisha_92@mail.ru>
2021-10-30 22:01:11 +0300

    Исправлены ошибки в test_add_group

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -7,10 +7,11 @@ import string
 
 def random_string(prefix, maxlen):
     symbols = string.ascii_letters + string.digits + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range (random.randrange(maxlen))])
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
 
 testdata = [Group(name="", header="", footer="")] + [
-    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string(("footer", 20)))
+    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
     for i in range(5)
 ]
 
