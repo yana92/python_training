4a2a375b857e18327ec65a81970c4db47c76343e
yana <kisha_92@mail.ru>
2021-11-15 11:25:44 +0300

    Исправлена инициализация Application после падения

--- conftest.py
+++ conftest.py
@@ -6,14 +6,13 @@ fixture = None
 @pytest.fixture
 def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseUrl")
     if fixture is None:
-        browser = request.config.getoption("--browser")
-        base_url = request.config.getoption("--baseUrl")
         fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
-            fixture = Application()
-            fixture.session.login(username="admin", password="secret")
+            fixture = Application(browser=browser, base_url=base_url)
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
 
