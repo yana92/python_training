205f2f228e8273a20bfa2ae97923559e60dfe599
yana <kisha_92@mail.ru>
2021-11-24 01:02:06 +0300

    Реализована отключаемая проверка пользовательского интерфейса для теста модификации группы

--- fixture/group.py
+++ fixture/group.py
@@ -68,6 +68,16 @@ class GroupHelper:
         self.return_to_groups_page()
         self.group_cache = None
 
+    def edit_group_by_id(self, id, new_group_data):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_group_by_id(id)
+        wd.find_element_by_name("edit").click()
+        self.fill_group_form(new_group_data)
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+        self.group_cache = None
+
     def select_first_group(self):
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
