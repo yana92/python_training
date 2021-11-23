1a5eec1ab229c357873df1c87b2a57ec0b4f6e77
yana <kisha_92@mail.ru>
2021-11-23 11:55:22 +0300

    Реализована загрузка тестовых данных из файла json для теста test_add_contact

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-#import pytest
 
 
-#@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, data_contacts):
-    contact = data_contacts
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create_contact(contact)
     app.return_to_main_page()
