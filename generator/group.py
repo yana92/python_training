75c1be8b3b1a432a435db2f80271af7c88c10865
yana <kisha_92@mail.ru>
2021-11-23 10:59:43 +0300

    Реализована загрузка тестовых данных из файла json для теста test_add_group

--- generator/group.py
+++ generator/group.py
@@ -2,7 +2,7 @@ from model.group import Group
 import random
 import string
 import os.path
-import json
+import jsonpickle
 import getopt
 import sys
 
@@ -24,7 +24,7 @@ for o, a in opts:
 
 
 def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*10
+    symbols = string.ascii_letters + string.digits
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
@@ -35,5 +35,6 @@ testdata = [Group(name="", header="", footer="")] + [
 
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
-with open(file,"w") as out:
-    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+with open(file, "w") as out:
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
