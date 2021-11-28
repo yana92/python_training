5ba2ae446a616b395fdeea4ebff543a35bfba3cc
yana <kisha_92@mail.ru>
2021-11-28 23:03:07 +0300

    Добавлено сравнение списка контактов на главной странице и в базе данных test_info_from_home_page_and_db

--- test/test_contacts_info.py
+++ test/test_contacts_info.py
@@ -20,6 +20,14 @@ def test_contacts_info_on_home_page(app):
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
     assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
 
+def test_info_from_home_page_and_db(app, db):
+    if app.contact.count() == 0:
+        app.contact.create_contact(Contact(lastname="Test Lastname", firstname="Firstname", address="Test addres",
+                                           home_phone="+74951111111", mobile="+79999999999", work_phone="Test Work",
+                                           email="Test email", phone2="Home"))
+    assert sorted(db.get_contact_list(), key=Contact.id_or_max) == sorted(
+        app.contact.get_contact_list(), key=Contact.id_or_max)
+
 
 def clear(s):
     return re.sub("[() -]", "", s)
@@ -33,3 +41,4 @@ def merge_phones_like_on_home_page(contact):
 def merge_emails_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x!= "",
                      [contact.email, contact.email2, contact.email3]))
+
