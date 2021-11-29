dfee6332a757f63bb9d8aa083b224915328eb590
yana <kisha_92@mail.ru>
2021-11-30 00:08:49 +0300

    Добавлено сравнение всех полей контактов для главной страницы и бд

--- test/test_contacts_info.py
+++ test/test_contacts_info.py
@@ -20,14 +20,21 @@ def test_contacts_info_on_home_page(app):
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
     assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
 
-def test_info_from_home_page_and_db(app, db):
+def test_contacts_from_home_page_and_db(app, db):
     if app.contact.count() == 0:
         app.contact.create_contact(Contact(lastname="Test Lastname", firstname="Firstname", address="Test addres",
                                            home_phone="+74951111111", mobile="+79999999999", work_phone="Test Work",
                                            email="Test email", phone2="Home"))
-    assert sorted(db.get_contact_list(), key=Contact.id_or_max) == sorted(
-        app.contact.get_contact_list(), key=Contact.id_or_max)
-
+    contacts_from_home_page = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+    contacts_from_db = sorted(db.get_contact_list(), key=Contact.id_or_max)
+    for i in range (len(contacts_from_home_page)):
+        assert contacts_from_home_page[i].lastname == contacts_from_db[i].lastname
+        assert contacts_from_home_page[i].firstname == contacts_from_db[i].firstname
+        assert contacts_from_home_page[i].address == contacts_from_db[i].address
+        assert contacts_from_home_page[i].all_phones_from_home_page == \
+               merge_phones_like_on_home_page(contacts_from_db[i])
+        assert contacts_from_home_page[i].all_emails_from_home_page == \
+               merge_emails_like_on_home_page(contacts_from_db[i])
 
 def clear(s):
     return re.sub("[() -]", "", s)
